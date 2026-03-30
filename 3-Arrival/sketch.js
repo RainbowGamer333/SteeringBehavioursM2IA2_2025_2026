963082403f7381bd1762a736126c110313d38902
Romain PATUREAU <romain.patureau@outlook.com>
2026-03-30 16:39:12 +0200

    Word mode

--- 3-Arrival/sketch.js
+++ 3-Arrival/sketch.js
@@ -1,5 +1,6 @@
 let target;
 let vehicles = [];
+let points = [];
 
 
 // Appelée avant de démarrer l'animation
@@ -13,9 +14,12 @@ function setup() {
 
   // La cible, ce sera la position de la souris
   target = createVector(random(width), random(height));
-
-  // on cree des vehicules, autant que de points
-  creerVehicules(4);
+  creerVehicules(20);
+  for (let i = 100; i < 200; i+= 25) {
+    for (let j = 100; j < 200; j+= 20) {
+      points.push(createVector(i, j));
+    }
+  }
 }
 
 function creerVehicules(n) {
@@ -44,22 +48,40 @@ function draw() {
   ellipse(target.x, target.y, 32);
   pop();
 
+  // dessine une ligne entre chaque véhicule adjacents
+  // stroke(255, 150);
+  // strokeWeight(2);
+  // noFill();
+  // for (let i = 0; i < vehicles.length - 1; i++) {
+  //   line(vehicles[i].pos.x, vehicles[i].pos.y, vehicles[i + 1].pos.x, vehicles[i + 1].pos.y);
+  // }
+
   // si on a affaire au premier véhicule
   // alors il suit la souris (target)
   let steeringForce;
   for (let i = 0; i < vehicles.length; i++) {
-    if (i === 0) {
-      // le premier véhicule suit la souris avec arrivée
-       steeringForce = vehicles[i].arrive(target, 0);
-    } else {
-      // les autres véhicules suivent le premier véhicule avec arrivée
-      steeringForce = vehicles[i].arrive(vehicles[i - 1].pos, 0);
-    }
+    // Snake method
+    // if (i === 0) {
+    //   // le premier véhicule suit la souris avec arrivée
+    //    steeringForce = vehicles[i].arrive(target, 0);
+    // } else {
+    //   // les autres véhicules suivent le premier véhicule avec arrivée
+    //   steeringForce = vehicles[i].arrive(vehicles[i - 1].pos, 0);
+    // }
+    
+    // Follow points
+    steeringForce = vehicles[i].arrive(points[i % points.length], 0);
+    
     vehicles[i].applyForce(steeringForce);
     vehicles[i].update();
     vehicles[i].show();
   }
 
+  // for (let p of points) {
+  //   stroke(255, 0, 255);
+  //   strokeWeight(4);
+  //   point(p.x, p.y);
+  // }
 }
 
 function keyPressed() {
